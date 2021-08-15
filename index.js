5f017ea7d31630e6f5e26129b0779690d204731d
Alin Neagu <alinneagu10@gmail.com>
2021-08-15 15:34:26 +0300

    Animations.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ for (var i = 0; i < drumButtons.length; i++) {
   drumButtons[i].addEventListener("click", function () {
     var buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
   });
 }
 
@@ -13,6 +14,7 @@ for (var i = 0; i < drumButtons.length; i++) {
 
 document.addEventListener("keydown", function (event) {
   makeSound(event.key);
+  buttonAnimation(event.key);
 });
 
 function makeSound(key) {
@@ -63,4 +65,12 @@ function makeSound(key) {
       console.log(buttonInnerHTML);
       break;
   }
+}
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey.toLowerCase());
+  activeButton.classList.add("pressed");
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
